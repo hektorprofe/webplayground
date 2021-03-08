113a0b7b23e6bf2b8d1165ce6e2f00721ecfaa48
Hektor <hcostaguzman@gmail.com>
2021-03-08 23:25:33 +0100

    Revisión Django 3.1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# web-playground-curso-django-2
-Repositorio de la Web Playground del curso Django 2
+# Web Playground del Curso de Django
+
+Proyecto revisado y funcionando perfectamente con Python 3.9 y Django 3.1
+
+## Instalación con Pipenv
+
+```bash
+pipenv install
+```
+
+## Instalación sin Pipenv
+
+```bash
+pip install -r requirements.txt
+```
--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -1,7 +1,7 @@
 """webplayground URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -31,11 +31,10 @@ urlpatterns = [
     path('profiles/', include(profiles_patterns)),
     # Paths de Messenger
     path('messenger/', include(messenger_patterns)),
-    
+
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
